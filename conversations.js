d32e118e05b6260e6c6fd56ba5086a789f82524c
Simon Lacoursière <lacoursieresimon@gmail.com>
2015-10-14 00:12:38 -0400

    Possibility to update messages, sending of all conversations for a user on request, IsTyping upgrade.

--- conversations.js
+++ conversations.js
@@ -5,18 +5,33 @@ exports = module.exports = function(io, _, usersBySocket, socketsByUserId, conve
         conversation.id = guid();
         conversations[conversation.id] = conversation;
         _.each(conversation.users, function(user){ socketsByUserId[user.id].join(conversation.id); });
+        
         // TODO : Save conversation in MongoDB
-        // TODO : filter user ids instead of using the default 0 array index
-        io.to(socket.id).emit('set:conversationId', { id: conversation.id, userId: conversation.users[0].id });
+        
+        io.to(socket.id).emit('set:conversationId', 
+            { 
+                id: conversation.id, 
+                userId: _.find(conversation.users, function(u){ 
+                    return u.id != usersBySocket[socket.id].id; 
+                }).id 
+           });
         socket.broadcast.to(conversation.id).emit('create:conversation', conversation);
     });
     
     socket.on('get:conversations', function(){
         var user = usersBySocket[socket.id];
         if (user != null){
+            
             // TODO : Fetch MongoDB for conversations related to the user
-            // TODO : Join all fetched conversations
-            io.to(socket.id).emit('get:conversations', conversations);
+            
+            var userConvs = _.filter(conversations, function(conv){ 
+                return _.find(conv.users, function(u){
+                    return u.id == user.id; 
+                }) != undefined; 
+            });
+            console.log(JSON.stringify(userConvs));
+            _.each(userConvs, function(conversation){ socket.join(conversation.id); });
+            io.to(socket.id).emit('get:conversations', userConvs);
         }
     });
     
--- messages.js
+++ messages.js
@@ -6,14 +6,30 @@ exports = module.exports = function(io, _, usersBySocket, socketsByUserId, conve
         if (conversations[message.conversationId] != null){
             conversations[message.conversationId].messages.push(message);
             socket.broadcast.to(message.conversationId).emit('send:message', message);
+            message.status = 'sent';
+            io.to(socket.id).emit('update:message', message);
+            // TODO : Update conversation in MongoDB
+        }
+    });
+    
+    socket.on('update:message', function (message) {
+        if (conversations[message.conversationId] != null){
+
+            _.each(conversations[message.conversationId].messages, function(mess, index){
+               if (message.id == mess.id){
+                   conversations[message.conversationId].messages[index] = message;
+                   socket.broadcast.to(message.conversationId).emit('update:message', message);
+               }
+            });
+            
             // TODO : Update conversation in MongoDB
             // TODO : Update message's status to the sender
+            // TODO : Implement in UI to update message on read
         }
     });
     
     socket.on('isTyping', function (response) {
-        // TODO : Only send to the concerned room with socket.broadcast.to(conversation.id).emit()
-        socket.broadcast.emit('isTyping', response);
+        socket.broadcast.to(response.conversationId).emit('isTyping', response);
     });
     
   });
